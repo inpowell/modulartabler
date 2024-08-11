7ed41297890ee20c2588ede1813b038d6f2e29a9
Ian Powell <powell.ian.n@gmail.com>
2024-08-11 13:09:32 +1000

    Allow measure in rows for convert_tabular

--- R/tables_long.R
+++ R/tables_long.R
@@ -31,23 +31,24 @@ count_aggregate <- function(MT, data, ...) {
 #'   various measures.
 #' @param formula A formula describing the visual structure of the table. The
 #'   LHS of the formula corresponds to rows, and the RHS corresponds to columns.
-#'   The `.Measure` variable can be used where there are multiple measures.
-#'   Passed to `data.table::melt`.
+#'   The `.Measure` variable (or alternative in `measurename`) can be used where
+#'   there are multiple measures.
 #' @param measures Columns that correspond to measures wanted in the output
 #'   table. The names of the vector will be used as "pretty" names in the output
 #'   table.
-#' @param ... Passed to `data.table::melt`.
+#' @param measurename The placeholder name in `formula` for distinguishing
+#'   measures, where multiple measures are given. This name is also used as the
+#'   descriptor of measures if used as a row group.
 #' @param value_override (Optional.) If a supplementary table is needed that
 #'   does not come from the measure columns (e.g. whether a cell needs to be
 #'   suppressed), this argument overrides the value used when converting to
 #'   tabular format.
 #'
-#' @return `convert_tabular` returns a `data.table` in visual tabular form.
-#'   Column names are determined by `data.table::melt`, which defaults to the
-#'   various dimensions separated by an underscore.
+#' @return `convert_tabular` returns a tibble in visual tabular form.
 #' @export
 #' @md
-#' @importFrom tidyr build_wider_spec pivot_wider_spec unite replace_na
+#' @importFrom tidyr pivot_longer build_wider_spec pivot_wider_spec unite
+#'   replace_na
 #' @importFrom rlang .env
 #' @importFrom dplyr mutate select arrange across
 #' @importFrom tidyselect all_of
@@ -55,26 +56,50 @@ convert_tabular <- function(
     table,
     formula,
     measures = c('No.' = 'n'),
-    ...,
+    measurename = '.Measure',
     value_override) {
   rowterms <- all.vars(formula[[2]])
   colterms <- all.vars(formula[[3]])
-  hasMeasure <- '.Measure' %in% colterms
-  colterms <- setdiff(colterms, '.Measure')
+
+  if (measurename %in% colterms) {
+    hasMeasure <- TRUE
+    colterms <- setdiff(colterms, measurename)
+  } else if (measurename %in% rowterms) {
+    # Need to pivot measures from columns to rows
+    table <- pivot_longer(
+      table,
+      cols = all_of(unname(measures)),
+      names_to = measurename,
+      names_transform = \(x) factor(x, levels = measures, labels = names(measures)),
+      values_to = '.value'
+    )
+    measures <- '.value'
+    hasMeasure <- FALSE
+  } else {
+    if (length(measures) > 1L)
+      cli::cli_abort(
+        '{.var measures} must have length 1, or {.var {measurename}} must be specified in {.var formula}.',
+        class = c('tabular-modtab-error', 'modtab-error')
+      )
+    hasMeasure <- FALSE
+  }
+
   spec <- build_wider_spec(
     table,
     names_from = all_of(colterms),
     values_from = all_of(unname(measures))
   )
 
-  specmod <- spec |>
-    mutate(.valname = names(measures)[match(.value, measures)]) |>
-    unite('.name', c(all_of(colterms), '.valname'), sep = '_', remove = FALSE) |>
-    select(-.valname) |>
-    arrange(across(all_of(colterms)), match(.value, .env$measures))
+  if (hasMeasure) {
+    spec <- spec |>
+      mutate(.valname = names(measures)[match(.value, measures)]) |>
+      unite('.name', c(all_of(colterms), '.valname'), sep = '_', remove = FALSE) |>
+      select(-.valname) |>
+      arrange(across(all_of(colterms)), match(.value, .env$measures))
+  }
 
-  visualtab <- pivot_wider_spec(table, specmod, id_cols = all_of(rowterms))
+  visualtab <- pivot_wider_spec(table, spec, id_cols = all_of(rowterms))
 
   # Replace NaNs, etc. with NA
-  mutate(visualtab, across(all_of(.env$specmod$.name), \(x) replace_na(x, NA)))
+  mutate(visualtab, across(all_of(.env$spec$.name), \(x) replace_na(x, NA)))
 }
--- tests/testthat/test-tables_long.R
+++ tests/testthat/test-tables_long.R
@@ -27,7 +27,7 @@ test_that('convert_tabular converts a semi-long table correctly', {
     suppress = c(FALSE, TRUE, TRUE, rep(FALSE, 9L))
   )
 
-  expected <- dplyr::tibble(
+  expected_mcol <- dplyr::tibble(
     Row = gl(3, 1, 3, labels = c('A', 'B', 'C')),
     'X_No.' = 1:3,
     'X_%' = c(0, 1, 10),
@@ -39,11 +39,63 @@ test_that('convert_tabular converts a semi-long table correctly', {
     'W_%' = c(80, 90, 100)
   )
 
-  tabular <- convert_tabular(
+  tabular_mcol <- convert_tabular(
     semilong,
     Row ~ Column + .Measure,
     measures = c('No.' = 'n', '%' = 'percent')
   )
 
-  expect_equal(tabular, expected)
+  expect_equal(tabular_mcol, expected_mcol)
+
+  expected_mrow <- dplyr::tibble(
+    Row = gl(3, 2, 6, labels = c('A', 'B', 'C')),
+    Measure = forcats::as_factor(rep(c('No.', '%'), times = 3)),
+    'X' = c(1, 0, 2, 1, 3, 10),
+    'Y' = c(4, 20, 5, 30, 6, 40),
+    'Z' = c(7, 50, 8, 60, 9, 70),
+    'W' = c(10, 80, 11, 90, 12, 100)
+  )
+
+  tabular_mrow <- convert_tabular(
+    semilong,
+    Row + Measure ~ Column,
+    measures = c('No.' = 'n', '%' = 'percent'),
+    measurename = 'Measure'
+  )
+
+  expect_equal(tabular_mrow, expected_mrow)
+
+  expected_nom <- dplyr::tibble(
+    Row = gl(3, 1, 3, labels = c('A', 'B', 'C')),
+    'X' = 1:3,
+    'Y' = 4:6,
+    'Z' = 7:9,
+    'W' = 10:12,
+  )
+
+  tabular_nom <- convert_tabular(
+    semilong,
+    Row ~ Column,
+    measures = c('No.' = 'n')
+  )
+
+  expect_equal(tabular_nom, expected_nom)
+})
+
+test_that('convert_tabular only supports one measure if not in formula', {
+  semilong <- data.frame(
+    Row = gl(3, 1, 12, labels = c('A', 'B', 'C')),
+    Column = gl(4, 3, 12, labels = c('X', 'Y', 'Z', 'W')),
+    n = 1:12,
+    percent = c(0, 1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+  )
+
+  expect_error(
+    convert_tabular(
+      semilong,
+      Row ~ Column,
+      measures = c('No.' = 'n', '%' = 'percent')
+    ),
+    class = 'tabular-modtab-error'
+  )
 })
