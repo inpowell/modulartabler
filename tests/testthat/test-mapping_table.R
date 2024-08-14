4a01b8ce99ae77270441ad8709aeebc636fbcd39
Ian Powell <powell.ian.n@gmail.com>
2024-08-14 22:55:52 +1000

    Simplify tests for BaseMappingTable bindings

--- tests/testthat/test-mapping_table.R
+++ tests/testthat/test-mapping_table.R
@@ -1,36 +1,36 @@
-test_that("BaseMappingTable bindings work as expected", {
+test_that("BaseMappingTable counting and nullspace is correct", {
+  withr::local_package('dplyr')
   map <- tibble(
     Map = factor(c(1L, 2L, 3L, 4L, rep(5L, 2), rep(6L, 4)),
                  labels = c(LETTERS[1:4], 'C+D', 'Total')),
     raw = factor(LETTERS[c(1L:4L, 3L:4L, 1L:4L)])
   )
-
-  rawside <- tibble(raw = factor(LETTERS[1L:4L]))
-  tabside <- tibble(Map = factor(1L:6L, labels = c(LETTERS[1:4], 'C+D', 'Total')))
-
-  matrix <- rbind(
-    c(1L, 0L, 0L, 0L, 0L, 1L),
-    c(0L, 1L, 0L, 0L, 0L, 1L),
-    c(0L, 0L, 1L, 0L, 1L, 1L),
-    c(0L, 0L, 0L, 1L, 1L, 1L)
-  )
-
-  nullspace <- rbind(
-    c(0, 0, 1, 1, -1, 0),
-    c(1, 1, 1, 1, 0, -1)
-  )
-
   MT <- BaseMappingTable$new(map, 'raw', 'Map')
 
-  expect_equal(MT$map, map)
-  expect_equal(MT$matrix, matrix)
-  expect_equal(MT$mraw, rawside)
-  expect_equal(MT$mtab, tabside)
-  expect_equal(MT$data_cols, 'raw')
-  expect_equal(MT$raw_cols, 'raw')
-  expect_equal(MT$table_cols, 'Map')
-  expect_equal(MT$join_clause, dplyr::join_by('raw' == 'raw')) # Character is okay
-  expect_equal(MT$nullspace, nullspace)
+  test <- tibble(
+    raw = forcats::as_factor(rep(c('A', 'B', 'C', 'D'), times = 1:4))
+  )
+
+  expected_count <- tibble(
+    Map = forcats::as_factor(c('A', 'B', 'C', 'D', 'C+D', 'Total')),
+    n = c(1L, 2L, 3L, 4L, 7L, 10L)
+  )
+
+  expect_equal(MT$count_aggregate(test), expected_count)
+
+  # We don't need these matrices to be equal, we just need them to have the same
+  # rowspace. Equivalently, an invertible matrix X exists s.t. ns_obs = X %*% ns_exp
+  ns_obs <- MT$nullspace
+  ns_exp <- rbind(
+    c(1, 1, 1, 1, 0, -1),
+    c(0, 0, 1, 1, -1, 0)
+  )
+  candidate <- t(qr.solve(t(ns_exp), t(ns_obs)))
+
+  # This matrix is invertible?
+  expect_no_error(solve(candidate))
+  # This matrix satisfies the requirements
+  expect_equal(ns_obs, candidate %*% ns_exp)
 })
 
 test_that("RangeMappingTable bindings work as expected", {
