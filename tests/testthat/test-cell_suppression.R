27a50db2218455265b1c6714f7a88c7f569ff20e
PeterM74 <60593713+PeterM74@users.noreply.github.com>
2024-09-13 15:37:04 +1000

    Add additional test to cover FP solutions

--- tests/testthat/test-cell_suppression.R
+++ tests/testthat/test-cell_suppression.R
@@ -81,6 +81,39 @@ test_that("Correctly deals with constraints hidden by prior cells", {
   expect_identical(determine_cell_suppression(n, ns), soln)
 })
 
+test_that("Solves highly dimensional tables that may return non-integer LP solutions", {
+  # as.logical(solution) turns any non-zero x*, such as floating points, into TRUE
+  # See Issue #17 for further details
+  create_maps <- function(colname, max = 5) {
+    BaseMappingTable$new(tibble::tibble(!!colname := c(letters[1:max],
+                                                       letters[1:max]),
+                                        !!paste0(colname, "2") := c(LETTERS[1:max],
+                                                                    rep("Total", max))),
+                         colname, paste0(colname, "2"))
+  }
+
+  map <- do.call(MultiMappingTable$new, purrr::map(LETTERS[1:3], ~ create_maps(.x, max = 4)))
+
+  n <- c(1L, 35L, 37L, 31L, 104L, 2L, 36L, 27L, 9L, 74L, 38L, 37L, 24L,  29L, 128L,
+         22L, 25L, 35L, 2L, 84L, 63L, 133L, 123L, 71L, 390L, 40L, 30L, 31L, 6L,
+         107L, 3L, 39L, 28L, 24L, 94L, 32L, 15L, 18L, 2L, 67L, 35L, 10L, 10L,
+         32L, 87L, 110L, 94L, 87L, 64L, 355L, 21L, 6L, 9L, 24L, 60L, 16L, 36L,
+         30L, 23L, 105L, 39L, 22L, 19L, 35L, 115L, 9L, 16L, 17L, 37L, 79L, 85L,
+         80L, 75L, 119L, 359L, 33L, 19L, 34L, 33L, 119L, 10L, 24L, 29L, 25L, 88L,
+         36L, 6L, 26L, 17L, 85L, 22L, 29L, 26L, 16L, 93L, 101L, 78L, 115L, 91L,
+         385L, 95L, 90L, 111L, 94L, 390L, 31L, 135L, 114L, 81L, 361L, 145L, 80L,
+         87L, 83L, 395L, 88L, 80L, 88L, 87L, 343L, 359L, 385L, 400L, 345L, 1489L)
+
+  soln <- c(T, T, F, F, F, T, F, F, T, F, F, F, F, F, F, F, T, F, T, F, F, F, F,
+            F, F, T, T, F, F, F, T, F, F, T, F, F, T, F, T, F, F, F, F, F, F, F,
+            F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F,
+            F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, T, F, T, F, F, T,
+            F, T, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F,
+            F, F, F, F, F, F, F, F, F, F)
+
+  expect_identical(determine_cell_suppression(n, map$nullspace), soln)
+})
+
 test_that("suppress_secondary deals with an empty nullspace (#9)", {
   data <- seq(0, 10, by = 2)
   suppress <- c(FALSE, TRUE, TRUE, FALSE, FALSE, FALSE)
